c8e2180b22a66f9de82802064ed8a3f70caeeae7
baptiste_gfy <geffroy.baptiste@outlook.fr>
2020-09-22 01:49:48 +0200

    Update exo_19.rb

--- 03_exos_Ruby/exo_19.rb
+++ 03_exos_Ruby/exo_19.rb
@@ -1,10 +1,11 @@
 compter = 2
 
 mon_tableau = ["jean.dupont.#{compter}@email.fr"]
-compter = 4
+compter += 2
 while (compter <= 50)
 	
 	mon_tableau << "jean.dupont.#{compter}@email.fr"
 	compter+=2
 end
+
 puts mon_tableau

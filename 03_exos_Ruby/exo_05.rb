6df3af4b1e280ad465e021f903cf06cbdeea4f6a
baptiste_gfy <geffroy.baptiste@outlook.fr>
2020-09-22 00:45:35 +0200

    Update exo_05.rb

--- 03_exos_Ruby/exo_05.rb
+++ 03_exos_Ruby/exo_05.rb
@@ -1,46 +1,31 @@
-# dans l'ennocer on nous demande de dire ce que fais #{ } 
-# donc #{ } permet de concaténer le résultat a la suite du puts 
-
-# LES COMMENTAIRE DE CHACUNE DES LIGNES SE TROUVE JUSTE AU DESSUS DE LA LIGNE CONCERNEE
-
-# affiche ma phrase entre "..."
 puts "On va compter le nombre d'heures de travail à THP"
 
-# affiche le nombre d'heure travailler (550) ( resultat de 5*10*11)
-puts "Travail : #{10 * 5 * 11}"
+puts "En faisant T.H.P tu va travailler #{10 * 5 * 11}"
 
-#fais la conversion en minutes
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
-# affiche le teste entre "..."
-puts "Et en secondes ?"
+print "Et en secondes ? "
 
-# fais la conversion en secondes
-puts 10 * 5 * 11 * 60 * 60
+puts 10 * 5 * 11 * 60 * 60 + "sec"
 
-# affiche le texte entre "..."
-puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts  "c'est drole de compter le temps de travail, mais peux tu répondre bon a ses questions ?"
 
-# verifie cette affirmation et renvoie false 
+print "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
 puts 3 + 2 < 5 - 7
 
-# affiche ça fais combien 3+2 ? et #{3+2} afiche le résultat
-puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 3 + 2 ? Ca fait #{3 + 2}"
 
-# affiche ça fais combien 5-7 ? et #{5-7} affiche le résultat
-puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+puts "Ça fait combien 5 - 7 ? Ca fait #{5 - 7}"
 
-# affiche le texte entre "..."
-puts "Ok, c'est faux alors !"
+puts "Ok, donc c'est bien faux alors !"
 
-# affiche le texte entre "..."
+puts ""
+
 puts "C'est drôle ça, faisons-en plus :"
 
-# affiche le texte entre "..." et indique si cela est vraix ou faux grace à #{ }
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
 
-# affiche le texte entre "..." et indique si cela est vraix ou faux grace à #{ }
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
 
-# affiche le texte entre "..." et indique si cela est vraix ou faux grace à #{ }
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"

739a91db4f43e9e592227effd0c473fa63c66e05
baptiste_gfy <geffroy.baptiste@outlook.fr>
2020-09-22 01:39:04 +0200

    Update exo_15.rb

--- 03_exos_Ruby/exo_15.rb
+++ 03_exos_Ruby/exo_15.rb
@@ -1,12 +1,14 @@
-puts "en quelle année est tu née"
+require 'date'
+
+puts "En quelle année est tu née"
 année = gets.chomp.to_i
 
 compteur = année
 age = 0
 
 
-while (compteur <= 2019)
-	puts "tu avais #{age} ans en #{compteur}"
+while (compteur <= Date.today.year)
+	puts "Tu avais #{age} ans en #{compteur}"
 	compteur += 1
 	age += 1
 end
